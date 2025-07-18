8066cfe019f88ad57537fb6bfaf9c49e725e28a8
ilhamfauzan630 <fauzanilham630@gmail.com>
2025-07-18 23:05:42 +0700

    very very fix bug login page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,6 +15,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     skipLinkButton: document.getElementById('skip-link'),
   });
   
+  window.addEventListener('hashchange', async () => {
+    await app.renderPage();
+
+    Camera.stopAllStreams();
+  });
+  
   await app.renderPage();
 
   await registerServiceWorker();
@@ -22,9 +28,4 @@ document.addEventListener('DOMContentLoaded', async () => {
     // for demonstration purpose-only
   console.log('Berhasil mendaftarkan service worker.');
 
-  window.addEventListener('hashchange', async () => {
-    await app.renderPage();
-
-    Camera.stopAllStreams();
-  });
 });
